f543c8ba3451d9691769f2582ed3b7adf73bce5a
tamahst <tamahst000@gmail.com>
2024-12-08 16:59:52 +0900

    テキスト差分表示

--- js/script.js
+++ js/script.js
@@ -19,3 +19,74 @@ function resetText(textareaId, countId) {
   countSpan.textContent = "0";
   countSpan.style.color = "";
 }
+
+// TODO: 複数の差分を表示できるようにする
+// TODO: 改行、空白も結果に表示できるようにする
+function compareText() {
+  const text1 = document.getElementById("text1").value;
+  const text2 = document.getElementById("text2").value;
+
+  const diff = calculateDiff(text1, text2);
+  displayDiff(diff);
+}
+
+function calculateDiff(text1, text2) {
+  const chars1 = text1.split("");
+  const chars2 = text2.split("");
+  const diff = [];
+  let i = 0,
+    j = 0;
+
+  while (i < chars1.length || j < chars2.length) {
+    if (i >= chars1.length) {
+      diff.push({ type: "add", char: chars2[j] });
+      j++;
+    } else if (j >= chars2.length) {
+      diff.push({ type: "remove", char: chars1[i] });
+      i++;
+    } else if (chars1[i] !== chars2[j]) {
+      diff.push({ type: "remove", char: chars1[i] });
+      diff.push({ type: "add", char: chars2[j] });
+      i++;
+      j++;
+    } else {
+      diff.push({ type: "same", char: chars1[i] });
+      i++;
+      j++;
+    }
+  }
+
+  return diff;
+}
+
+function displayDiff(diff) {
+  const resultDiv1 = document.getElementById("result1");
+  resultDiv1.innerHTML = "";
+  const resultDiv2 = document.getElementById("result2");
+  resultDiv2.innerHTML = "";
+
+  let diffText1 = "";
+  let diffText2 = "";
+  diff.forEach((d) => {
+    switch (d.type) {
+      case "add":
+        diffText1 += `<span class="diff-add">${d.char}</span>`;
+        break;
+      case "remove":
+        diffText2 += `<span class="diff-remove">${d.char}</span>`;
+        break;
+      case "same":
+        diffText1 += `<span>${d.char}</span>`;
+        diffText2 += `<span>${d.char}</span>`;
+        break;
+    }
+  });
+
+  resultDiv1.innerHTML = `
+            <div>${diffText1}</div>
+  `;
+
+  resultDiv2.innerHTML = `
+          <div>${diffText2}</div>
+`;
+}
