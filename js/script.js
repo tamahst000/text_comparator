385a07e684b4ac0250c1c7ee1c28d1c1d1c7ff4f
tamahst <tamahst000@gmail.com>
2024-12-21 14:45:07 +0900

    Gitライクな差分表示

--- js/script.js
+++ js/script.js
@@ -33,75 +33,15 @@ function compareText() {
   const text1 = document.getElementById("text1").value;
   const text2 = document.getElementById("text2").value;
 
-  const diff = calculateDiff(text1, text2);
-  displayDiff(diff);
-}
+  const diff = JsDiff.createPatch("text1/text2", text1, text2);
 
-function calculateDiff(text1, text2) {
-  const chars1 = text1.split(/(\r\n|\n|\r|\s)/);
-  const chars2 = text2.split(/(\r\n|\n|\r|\s)/);
-  const diff = [];
-  let i = 0,
-    j = 0;
-
-  while (i < chars1.length || j < chars2.length) {
-    if (i >= chars1.length) {
-      diff.push({ type: "add", char: chars2[j] });
-      j++;
-    } else if (j >= chars2.length) {
-      diff.push({ type: "remove", char: chars1[i] });
-      i++;
-    } else if (chars1[i] !== chars2[j]) {
-      diff.push({ type: "remove", char: chars1[i] });
-      diff.push({ type: "add", char: chars2[j] });
-      i++;
-      j++;
-    } else {
-      diff.push({ type: "same", char: chars1[i] });
-      i++;
-      j++;
-    }
-  }
-
-  return diff;
-}
-
-function displayDiff(diff) {
-  const resultDiv1 = document.getElementById("result1");
-  resultDiv1.innerHTML = "";
-  const resultDiv2 = document.getElementById("result2");
-  resultDiv2.innerHTML = "";
-
-  let diffText1 = "";
-  let diffText2 = "";
-  diff.forEach((d) => {
-    switch (d.type) {
-      case "add":
-        diffText1 += `<span class="diff-add">${escapeHtml(d.char)}</span>`;
-        break;
-      case "remove":
-        diffText2 += `<span class="diff-remove">${escapeHtml(d.char)}</span>`;
-        break;
-      case "same":
-        diffText1 += `<span>${escapeHtml(d.char)}</span>`;
-        diffText2 += `<span>${escapeHtml(d.char)}</span>`;
-        break;
-    }
+  const diffHtml = Diff2Html.getPrettyHtml(diff, {
+    outputFormat: "side-by-side",
+    matching: "lines",
+    drawFileList: false,
+    fileListToggle: false,
+    fileContentToggle: false,
   });
 
-  resultDiv1.innerHTML = `<div>${diffText1}</div>`;
-  resultDiv2.innerHTML = `<div>${diffText2}</div>`;
-}
-
-function escapeHtml(str) {
-  const map = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#039;",
-    "\n": "<br>",
-    " ": "&nbsp;",
-  };
-  return str.replace(/[&<>"'\n\s]/g, (m) => map[m] || m);
+  document.getElementById("result").innerHTML = diffHtml;
 }
